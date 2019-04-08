e9fce7a649173bb8a0db1b674074e9ad318a37c5
濱野宏樹 <h-hamano@hamanohirokinoMacBook-puro.local>
2019-04-08 19:33:45 +0900

    PWA導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,8 +50,25 @@ export default {
   ** Nuxt.js modules
   */
   modules: [
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa'
   ],
+
+  manifest: {
+    name: 'nuxt-start',
+    lang: 'ja',
+    short_name: 'nuxt',
+    title: 'nuxt-startです',
+    'og:title': 'nuxt-startです',
+    description: 'nuxt-startです',
+    'og:description': 'nuxt-startです',
+    theme_color: '#ffffff',
+    background_color: '#ffffff'
+  },
+
+  workbox: {
+    dev: true, //開発環境でもPWA
+  },
 
   axios: {
 
